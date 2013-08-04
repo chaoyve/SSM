435f06b60c28f6fc1ca084e12f2c26f8d8b2aa46
yi.cui.clxy <j_clxy@hotmail.com>
2013-08-04 10:53:29 +0800

    Add start listener before spring servlet.

---
+++ src/main/java/cn/clxy/ssm/common/web/WebAppListener.java
@@ -0,0 +1,19 @@
+package cn.clxy.ssm.common.web;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class WebAppListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+
+		String rootPath = sce.getServletContext().getRealPath("/");
+		System.setProperty("webroot", rootPath);
+
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+	}
+}
